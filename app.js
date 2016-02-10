280e1553de985a79c283c40fb9a7042d8d74554d
recklessmoxie <shellybarkley@ymail.com>
2016-02-10 09:58:50 -0800

    corrected comment format on index.html and added beginning content to app.js.

---
+++ app.js
@@ -0,0 +1,12 @@
+
+var userName = prompt('What is your name?');
+console.log('The user said their name is ' + userName );
+alert('Hi there, ' + userName ', I am going to ask you some questions abou me. ' + ' Please answer Y or N');
+
+var answerOne = prompt('Was Shelly born in Seattle?');
+if (answerOne === 'N'||'n') {
+alert('You are correct, unfortunately Shelly was born in Tulsa Oklahoma.');
+console.log(userName + 'Guessed where I was born correctly');
+
+  } else if {}
+  }
