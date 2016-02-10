0e9672e265b6d283537f684d4226cd6bab206889
recklessmoxie <shellybarkley@ymail.com>
2016-02-10 11:36:58 -0800

    Added additional q/a and if/else

--- app.js
+++ app.js
@@ -3,25 +3,25 @@
 
 var userName = prompt('What is your name?');
   console.log('The user said their name is ' + userName );
-  alert('Hi there, ' + userName + ', I am going to ask you some questions abou me. ' + ' Please answer Y or N');
+  alert('Hi there, ' + userName + ', I am going to ask you some questions about me. ' + ' Please answer Y or N');
 
 var answer1 = prompt('Was Shelly born in Tulsa, Oklahoma?');
-  console.log('The user answer to Question 1 was ' + answer1);
+  console.log(userName 'answered Question 1 with ' + answer1);
 
 if (answer1.toLowerCase() === 'y' || answer1.toUpperCase() === 'YES') {
   alert('That is correct! ' + ' Shelly was born in Tulsa, Oklahoma.');
   console.log(userName + ' totally nailed Question 1');
 
 } else if (answer1.toLowerCase() === 'n' || answer1.toUpperCase() === 'NO') {
-  alert('Sorry, you\'re wrong!!!');
+  alert('Sorry ' + userName + ' you\'re wrong!!!');
   console.log(userName + ' did not answer Question 1 correctly');
 
 } else {
   alert('You should have given a Y or N answer, so you lose this round.');
-};
+}
 
 var answer2 = prompt('Does Shelly have any siblings?');
-  console.log('The user answer to Question 2 was ' + answer2);
+  console.log(userName 'answered Question 2 with ' + answer2);
 
 if (answer2.toLowerCase() === 'y' || answer2.toUpperCase() === 'YES') {
   alert('Yes, Shelly has 1 older sister.');
@@ -33,4 +33,22 @@ if (answer2.toLowerCase() === 'y' || answer2.toUpperCase() === 'YES') {
 
 } else {
   alert('You really should work on following directions better!');
-};
+}
+
+var answer3 = prompt('Is Shelly older than 18?');
+  console.log(userName + ' answered Question 3 with' + answer3);
+
+if (answer3.toLowerCase() === 'y' || answer3.toUpperCase() === 'YES') {
+  alert('That is the correct answer, nice job! ' + userName);
+  console.log(userName + ' got the answer to Question 3 right and chose ' + answer3);
+
+} else if (answer3.toLowerCase() === 'n' || answer3.toUpperCase() === 'NO') {
+  alert('Sorry ' + userName + answer3 ' is not the correct answer, but Shelly would be flattered with your response.');
+  console.log(userName + 'answered question 3 with' + answer3 + ' Which is not correct.');
+
+} else {
+  alert('It is really unfortunate that you do not grasp how to play the game.');
+}
+}
+
+}
