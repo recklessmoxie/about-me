160c5837896ee366a6f8475e75350169edb4792e
recklessmoxie <shellybarkley@ymail.com>
2016-02-10 10:18:40 -0800

    Flushed out questions and answers, added if/else statements.

--- app.js
+++ app.js
@@ -4,9 +4,15 @@ console.log('The user said their name is ' + userName );
 alert('Hi there, ' + userName ', I am going to ask you some questions abou me. ' + ' Please answer Y or N');
 
 var answerOne = prompt('Was Shelly born in Seattle?');
-if (answerOne === 'N'||'n') {
-alert('You are correct, unfortunately Shelly was born in Tulsa Oklahoma.');
-console.log(userName + 'Guessed where I was born correctly');
 
-  } else if {}
+if (answerOne === 'n.toUpperCase') {
+alert('You are correct. ' + ' Unfortunately, Shelly was born in Tulsa Oklahoma.');
+console.log(userName + 'Guessed where I was born correctly. ' + (' First Question'));
+
+  } else if (answerOne === 'y.toUpperCase')
+    alert('You are incorrect ' + ' Shelly was born in Tulsa, Oklahoma.');
+  {
+
+
+  }
   }
