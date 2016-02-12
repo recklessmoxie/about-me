6d8a1041e580de5360464c1adac7906e0364a96b
rashidbaset <rashid.baset@gmail.com>
2016-02-12 08:54:28 -0800

    Added function for question 2.

--- app.js
+++ app.js
@@ -7,43 +7,45 @@ console.log('The user said their name is ' + userName );
 alert('Hi there, ' + userName + ', I am going to ask you some questions about me. ' + ' Please answer Y or N');
 
 
-function question1(){
-  var answer1 = prompt('Was I born in Tulsa, Oklahoma?');
-  console.log(userName + ' answered Question 1 with ' + answer1);
+// function question1(){
+//   var answer1 = prompt('Was I born in Tulsa, Oklahoma?');
+//   console.log(userName + ' answered Question 1 with ' + answer1);
+//
+//   if (answer1.toLowerCase() === 'y' || answer1.toUpperCase() === 'YES') {
+//     alert('That is correct! ' + ' I was born in Tulsa, Oklahoma.');
+//     console.log(userName + ' totally nailed Question 1');
+//
+//   } else if (answer1.toLowerCase() === 'n' || answer1.toUpperCase() === 'NO') {
+//     alert('Sorry ' + userName + ' you\'re wrong!!!');
+//     console.log(userName + ' did not answer Question 1 correctly');
+//
+//   } else {
+//     alert('You should have given a Y or N answer, so you lose this round.');
+//   }
+// }
+//
+// question1();
 
-  if (answer1.toLowerCase() === 'y' || answer1.toUpperCase() === 'YES') {
-    alert('That is correct! ' + ' I was born in Tulsa, Oklahoma.');
-    console.log(userName + ' totally nailed Question 1');
 
-  } else if (answer1.toLowerCase() === 'n' || answer1.toUpperCase() === 'NO') {
-    alert('Sorry ' + userName + ' you\'re wrong!!!');
-    console.log(userName + ' did not answer Question 1 correctly');
+function question2() {
+  var answer2 = prompt('Do I have any siblings?');
+  console.log(userName + ' answered Question 2 with ' + answer2);
+
+  if (answer2.toLowerCase() === 'y' || answer2.toUpperCase() === 'YES') {
+    alert('Yes, I have an older sister.');
+    console.log(userName + ' answered Question 2 correctly');
+
+  } else if (answer2.toLowerCase() === 'n' || answer2.toUpperCase() === 'NO') {
+    alert('Sorry that is incorrect, I happen to have an older sister.');
+    console.log(userName + ' answered Question 2 incorrectly');
 
   } else {
-    alert('You should have given a Y or N answer, so you lose this round.');
+    alert('You really should work on following directions better!');
   }
 }
 
-question1();
+question2();
 
-
-//
-// var answer2 = prompt('Do I have any siblings?');
-// console.log(userName + ' answered Question 2 with ' + answer2);
-//
-// if (answer2.toLowerCase() === 'y' || answer2.toUpperCase() === 'YES') {
-//   alert('Yes, I have an older sister.');
-//   console.log(userName + ' answered Question 2 correctly');
-//
-// } else if (answer2.toLowerCase() === 'n' || answer2.toUpperCase() === 'NO') {
-//   alert('Sorry that is incorrect, I happen to have an older sister.');
-//   console.log(userName + ' answered Question 2 incorrectly');
-//
-// } else {
-//   alert('You really should work on following directions better!');
-// }
-//
-//
 // var answer3 = prompt('Am I older than 18?');
 // console.log(userName + ' answered Question 3 with ' + answer3);
 //
